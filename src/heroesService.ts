a7409bfd0d4263b2f182ef3ce84831ea27d6f193
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-09-11 16:32:30 +0200

    chore: initialize without constructor

--- src/heroesService.ts
+++ src/heroesService.ts
@@ -1,11 +1,7 @@
 import {Hero} from './hero';
 
 export class HeroesService {
-  heroes: Array<Hero>;
-
-  constructor() {
-    this.heroes = [{ name: "Superman" }, { name: "Batman" }]
-  }
+  heroes: Array<Hero> = [{ name: "Superman" }, { name: "Batman" }]
 
   getHeroes () {
     return this.heroes;
